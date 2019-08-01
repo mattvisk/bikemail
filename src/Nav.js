ad977a2267405f47107340cd0cf3a088b7407cb0
mattvisk <mattvisk@gmail.com>
2019-07-31 22:13:06 -0400

    Router Tutorial Part 1
    https://www.youtube.com/watch?v=Law7wfdg_ls&t=64s

---
+++ src/Nav.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import {Link} from 'react-router-dom';
+
+function Nav() {
+
+    const navStyle = {
+        color: 'white'
+    };
+
+    return (
+        <nav>
+            <Link to="/"><h1>Logo</h1></Link>
+            <ul>
+                <Link to="/about"><li>About</li></Link>
+                <Link to="/shop"><li>Shop</li></Link>
+            </ul>
+        </nav>
+    );
+}
+
+export default Nav;
---
+++ src/Shop.js
@@ -0,0 +1,31 @@
+import React, {useState, useEffect} from 'react';
+import './App.css';
+
+function Shop() {
+
+    useEffect(()=>{
+        fetchItems();
+    },[]);
+
+    // idk
+    const [items, setItems] = useState([]);
+
+    // Fetch Data
+    const fetchItems = async()=>{
+        const data = await fetch('https://fortnite-public-api.theapinetwork.com/prod09/upcoming/get'); 
+        const items = await data.json();
+        console.log(items.items);
+        setItems(items.items);
+    }
+
+
+    return (
+        <div>
+            {items.map(item => {
+                <h1 key={item.itemid}>{item.name}</h1>
+            })} 
+        </div> 
+    );
+}
+
+export default Shop;
