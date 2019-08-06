86e4b12d331f579ed72fda4fe238dfd8fcf3ae95
mattvisk <mattvisk@gmail.com>
2019-08-06 02:51:21 -0400

    Signup Page HTML

--- src/Signup.js
+++ src/Signup.js
@@ -5,8 +5,23 @@ import './assets/css/animations.css';
 function Signup() {
     return (
         <div class="container">
-            
-            <form class="centerForm signup">
+            <div class="center title">
+                <h1>Create an Account</h1>
+            </div>
+            <form class="center-form signup">
+                <p>
+                    <label>Name</label>
+                    <input name="name"/>
+                </p>
+                <p>
+                    <label>Email</label>
+                    <input name="email"/>
+                </p>
+                <p>
+                    <label>Create Password</label>
+                    <input type="password" name="password"/>
+                </p>
+                <button class="btn" type="submit">Get Started!</button>
             </form>
         </div>
     );
