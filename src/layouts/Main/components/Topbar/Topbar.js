1a31736833f80f875b4835a4ab0b9c8cc5cd1b88
web <kd19850511@gmail.com>
2019-08-15 12:38:28 -0500

    add active and fix css issue

--- src/layouts/Main/components/Topbar/Topbar.js
+++ src/layouts/Main/components/Topbar/Topbar.js
@@ -2,6 +2,7 @@ import React ,{ forwardRef }  from 'react';
 import {NavLink} from 'react-router-dom';
 import {  Button } from '@material-ui/core';
 import { connect } from 'react-redux'
+import { makeStyles } from '@material-ui/styles';
 import AccountBoxIcon from '@material-ui/icons/AccountBox';
 import logo from './logo_v1.png';
 import './topbar.scss';
@@ -12,6 +13,23 @@ import {
 const headerstyle = {
   zIndex: 5,
 };
+const useStyles = makeStyles(theme => ({
+  active_link: {
+    borderBottom: "2px solid #ddd"
+  },
+  bold: {
+    background: '#2229ab',
+    borderRadius: 2,
+    color: '#fff',
+    marginLeft: 10,
+    marginRight: 10
+  },
+  headerLink: {
+    marginLeft: 10,
+    marginRight: 10
+
+  }
+}));
 const CustomRouterLink = forwardRef((props, ref) => (
   <div
     ref={ref}
@@ -21,6 +39,7 @@ const CustomRouterLink = forwardRef((props, ref) => (
 ));
 const Topbar = props => {
     const {history} = props
+    const classes = useStyles();
     console.log('Topbar',props);
     if(props.isLoggedIn == ''){
 
@@ -46,18 +65,18 @@ const Topbar = props => {
 
                               <li>
                                   <Button
-                                      className = 'headerlink'
-                                      activeClassName = 'active_link'
+                                      className = {classes.headerLink}
+                                      activeClassName = {classes.active_link}
                                       component={CustomRouterLink}
                                       to='/email-templates'
                                     >
                                   Email Templates
                                 </Button>
                               </li>
-                              <li><a>|</a></li>
+                              <li  className='splitter'><a>|</a></li>
                               <li><Button
-                                className = 'headerlink'
-                                activeClassName = 'active_link'
+                                className = {classes.headerLink}
+                                activeClassName = {classes.active_link}
 
                               >
                                 <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M12 5.9c1.16 0 2.1.94 2.1 2.1s-.94 2.1-2.1 2.1S9.9 9.16 9.9 8s.94-2.1 2.1-2.1m0 9c2.97 0 6.1 1.46 6.1 2.1v1.1H5.9V17c0-.64 3.13-2.1 6.1-2.1M12 4C9.79 4 8 5.79 8 8s1.79 4 4 4 4-1.79 4-4-1.79-4-4-4zm0 9c-2.67 0-8 1.34-8 4v3h16v-3c0-2.66-5.33-4-8-4z"/></svg>
@@ -66,9 +85,10 @@ const Topbar = props => {
                               </Button>
                               </li>
                               <li><Button
-                                className = 'headerlink'
+                                className = {classes.headerLink}
                                 onClick={props.signout}
-                                activeClassName = 'active_link'
+                                activeClassName = {classes.active_link}
+                                
 
                               >
                                                   <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M10.09 15.59L11.5 17l5-5-5-5-1.41 1.41L12.67 11H3v2h9.67l-2.58 2.59zM19 3H5c-1.11 0-2 .9-2 2v4h2V5h14v14H5v-4H3v4c0 1.1.89 2 2 2h14c1.1 0 2-.9 2-2V5c0-1.1-.9-2-2-2z"/></svg>
@@ -79,8 +99,8 @@ const Topbar = props => {
                               <ul>
                               <li>
                                   <Button
-                                      className = 'headerlink'
-                                      activeClassName = 'active_link'
+                                      className = {classes.headerLink}
+                                      activeClassName = {classes.active_link}
 
                                       component={CustomRouterLink}
                                       to='/products-and-pricing'
@@ -88,11 +108,12 @@ const Topbar = props => {
                                   Pricing
                                 </Button>
                               </li>
-                              <li><a>|</a></li>
+                              <li className='splitter'><a>|</a></li>
                               <li><Button
-                                className = 'headerlink'
+                                className = {classes.headerLink}
                                 component={CustomRouterLink}
-                                activeClassName = 'active_link'
+                                activeClassName = {classes.active_link}
+                                
 
                                 to='/sign-in'
                               >
@@ -101,9 +122,9 @@ const Topbar = props => {
                               </li>
                               <li>
                                  <Button
-                                  className = 'headerlink bold'
+                                  className = {classes.bold}
                                   component={CustomRouterLink}
-                                  activeClassName = 'active_link'
+                                  activeClassName = {classes.active_link}
 
                                   to='/products-and-pricing'
                                 >
