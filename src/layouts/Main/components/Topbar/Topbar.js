e318b0eb431775741b558b878ae51f635d6e2a2a
mattvisk <mattvisk@gmail.com>
2019-09-03 23:35:46 -0400

    nav styling

--- src/layouts/Main/components/Topbar/Topbar.js
+++ src/layouts/Main/components/Topbar/Topbar.js
@@ -4,6 +4,7 @@ import { NavLink } from 'react-router-dom';
 import { connect } from 'react-redux';
 import { makeStyles } from '@material-ui/styles';
 import AccountBoxIcon from '@material-ui/icons/AccountBox';
+import Icon from '@material-ui/core/Icon';
 import logo from './logo_v1.png';
 import './topbar.scss';
 import { SIGNOUT, LOGEDIN } from '../../../../modules/user';
@@ -33,17 +34,22 @@ const Topbar = props => {
           </NavLink>
           {props.isLoggedIn == true ? (
             <nav>
-              <NavLink to="/recipients">Recipients</NavLink>
-              <NavLink to="/email-templates"> Email Templates </NavLink>
-              <NavLink to="/account"> {props.username} </NavLink>
-              <NavLink onClick={signOut}>Logout</NavLink>
+              <NavLink to="/email-templates"> <Icon>star</Icon> Emails </NavLink>
+              <NavLink to="/recipients"><Icon>star</Icon> Recipients</NavLink>
+              <NavLink to="/settings"> <Icon>star</Icon> Settings </NavLink>
+              <div className="small-links">
+                <NavLink to="/account"> {props.username} </NavLink>
+                <NavLink onClick={signOut}>Logout</NavLink>
+              </div>
             </nav>
           ) : (
             <nav>
               <NavLink to="/create-an-account" className='special'> Create an account </NavLink>
               <NavLink to="/products-and-pricing"> Pricing </NavLink>
               <NavLink to="/contact"> Contact </NavLink>
-              <NavLink to="/sign-in"> Login </NavLink>
+              <div className="small-links">
+                <NavLink to="/sign-in"> Login </NavLink>
+              </div>
             </nav>
           )}
         </div>
