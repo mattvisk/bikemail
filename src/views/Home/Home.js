fe9fa94c596439fd1a75e24de72e0154a0e33ca6
mattvisk <mattvisk@gmail.com>
2019-09-12 11:58:16 -0400

    Top nav darkened

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -10,9 +10,7 @@ const Home = () => {
       <img width="400px" alt="envelope" src={envelope} />
       <h1>Automatic Customer Communication.</h1>
       <h2>Talk to your customers one-on-one, at scale.</h2>
-
       <NavLink className="large-btn" to='/create-an-account'>Create your account</NavLink>
-
     </div>
   );
 };
