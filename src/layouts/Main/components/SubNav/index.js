4aa7ab4292bc075670704a4f27de29631e4630e4
mattvisk <mattvisk@gmail.com>
2019-09-21 01:55:44 -0400

    subnav styling

--- src/layouts/Main/components/SubNav/index.js
+++ src/layouts/Main/components/SubNav/index.js
@@ -2,16 +2,18 @@ import React, { forwardRef } from 'react';
 import { matchPath } from 'react-router';
 import { NavLink } from 'react-router-dom';
 import Styles from './subnav.scss';
+import ViewComfyIcon from '@material-ui/icons/ViewComfy';
+import GroupIcon from '@material-ui/icons/Group';
 
 
 const SubNav = props => {
   return(
     <div className={Styles}>
-        <ul>
-            <NavLink to="email-campaigns">Series<small>Courses, Newsletters, Etc.</small></NavLink>
-            <NavLink to="email-campaigns">One Time<small>Courses, Newsletters, Etc.</small></NavLink>
-            <NavLink to="email-campaigns">Action<small>Courses, Newsletters, Etc.</small></NavLink>
-        </ul>
+      <ul>
+        <NavLink to="email-campaigns"><ViewComfyIcon/><span>Series<small>Courses, Newsletters, Etc.</small></span></NavLink>
+        <NavLink to="email-campaigns"><GroupIcon/> <span>One Time<small>Courses, Newsletters, Etc.</small></span></NavLink>
+        <NavLink to="email-campaigns"><GroupIcon/> <span>Action<small>Courses, Newsletters, Etc.</small></span></NavLink>
+      </ul>
     </div>
   )
 };
